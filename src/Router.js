96782bf5a14bc1d3389d9e621f868f7274169ac8
Alex Marolleau <Alex.M79@outlook.com>
2024-05-19 11:49:05 +0200

    Home page + Scss

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Layout from './components/Layout';
@@ -9,6 +10,7 @@ function AppRouter() {
     <Router>
       <Layout>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/Home.scss';
+import logements from '../monFichier.json';
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="banner">
+        <h1>Chez vous, partout et ailleurs</h1>
+      </div>
+      <div className="logements-grid">
+        {logements.map(logement => (
+          <div className="logement-card" key={logement.id}>
+            <img src={logement.cover} alt={logement.title} />
+            <div className="logement-title">
+              <h2>{logement.title}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
